77b27784383572b4d59692a8dfc941e1f5688e88
Henrique Naimaier <coubron@yahoo.com.br>
2022-02-01 14:55:29 -0300

    Embeddable

--- src/main/java/br/com/alura/loja/modelo/Cliente.java
+++ src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.modelo;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,12 +14,11 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
-	private String cpf;
+	@Embedded
+	private DadosPessoais dadosPessoais;
 	
 	public Cliente(String nome, String cpf) {
-		this.nome = nome;
-		this.cpf = cpf;
+		this.dadosPessoais = new DadosPessoais(nome, cpf);
 	}
 	
 	public Cliente() {
@@ -33,20 +33,11 @@ public class Cliente {
 	}
 
 	public String getNome() {
-		return nome;
+		return this.dadosPessoais.getNome();
 	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
+	
 	public String getCpf() {
-		return cpf;
+		return this.dadosPessoais.getCpf();
 	}
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	
-	
 }
---
+++ src/main/java/br/com/alura/loja/modelo/DadosPessoais.java
@@ -0,0 +1,31 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class DadosPessoais {
+	private String nome;
+	private String cpf;
+	
+	public DadosPessoais(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	public DadosPessoais() {
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+}
